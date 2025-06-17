1a749f5bbfefbaf4fac0bd0a329d49c224b3aae4
Savi33-web <savinozerk99@gmail.com>
2025-06-17 09:49:05 +0000

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *main - prints "Programming is like building a multilingual text
+ *main - prints "Programming is like building a multilingual puzzle
  *Return: Always 0 (Succes)
  */
 int main(void)
 {
-         puts("\"Programming is like building a multilingual text")
+         puts("\"Programming is like building a multilingual puzzle")
          return (0);
 }
