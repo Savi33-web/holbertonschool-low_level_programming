436abbc971aadbcbe148892ffa87d177eb257c21
Savi33-web <savinozerk99@gmail.com>
2025-06-17 08:15:19 +0000

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-puts <"Programming is like building a multilingual puzzle_0>|. /n|gcc -o "Programming is like building a multilingual puzzle 
+puts <Programming is like building a multilingual puzzle_0>; gcc -o Programming is like building a multilingual puzzle |/n| grep Programming is like buldiing a multilingual puzzle.
