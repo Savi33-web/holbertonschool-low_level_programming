b52789da9fb6bee8a59b3e824838b1eeaf78ffb9
Savi3-web <savinozerk99@gmail.com>
2025-06-17 23:08:11 +0000

    test

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,8 +7,22 @@
  */
 int main(void)
 {
-  int bytes_int = 4;
-  long long bytes_8 = 8;
-  char bytes_char = 1;
-  return (0);
-}  
+  Size of a char = 1 bytes;
+  Size of a int = 4 bytes;
+  Size of a long int = 4 bytes;
+  Size of a long long int = 8 bytes;
+  Size of a float = 4 bites;
+
+  /**
+   * %d : nombre entier (int)
+   * %f : nombre flottant (float)
+   * %c : character (char)
+   */
+
+    printf("Size of a char: %lu byte(s)\n", sizeof(char));
+    printf("Size of an int: %lu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+}
