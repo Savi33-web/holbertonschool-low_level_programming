55cf98eb6672ae1b150cbecb6ccc19118b67f10e
Savi33-web <savinozerk99@gmail.com>
2025-06-19 21:35:45 +0000

    test

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
         last_digit = n % 10;
     if(last_digit > 5)
     {
-      printf("last_digit of %d is %d and is greater than 5%\n" , n , last_digit);
+      printf("last_digit of %d is %d and is greater than 5\n" , n , last_digit);
     }
     else if(last_digit ==0)
     {
@@ -25,5 +25,5 @@ int main(void)
        printf("last_digit of %d is %d and is less than 6 and not 0\n" , n , last_digit);
     }
 
-    Return (0);
+    return (0);
 }
