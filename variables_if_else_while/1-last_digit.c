7a39d16d48e62961f463f6464a71b93eaf170521
Savi33-web <savinozerk99@gmail.com>
2025-06-19 22:06:55 +0000

    test

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,6 +16,11 @@ int main(void)
     int n;
     int last_digit;
 
+    last_digit = n % 10;
+    if (last_digit < 0)
+	last_digit = -last_digit;
+
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
         last_digit = n % 10;
