aea0edfc4178082acd8c7fa23f8eea962d2bc0c1
Savi33-web <savinozerk99@gmail.com>
2025-06-20 07:51:24 +0000

    test

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,11 @@
  *Return: Always 0 (Success)
  */
  int main(void)
+{
+ char letter;  
  for (char letter ='a' , char letter<='z', letter++);
  putchar(letter);
-{
-putchar('\n');
+
+ putchar('\n');
  return (0);
 } 
