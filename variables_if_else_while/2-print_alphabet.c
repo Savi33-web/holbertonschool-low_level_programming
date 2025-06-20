5c1a78819cfc3c99d1a09596a8ac57c48f3de447
Savi33-web <savinozerk99@gmail.com>
2025-06-20 07:27:01 +0000

    test

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+{
+putchar("Write a program that prints the alphabet in lowercase, followed by a new line.\n");
+ return (0);
+} 
