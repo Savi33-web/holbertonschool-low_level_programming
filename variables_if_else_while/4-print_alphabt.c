9b5f0da95ebc5c30af6e8876f75652021365c8c0
Savi33-web <savinozerk99@gmail.com>
2025-06-20 09:25:31 +0000

    test

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,9 @@ int main (void)
   {
     char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter !='e'; letter !='q'; letter++)
+    for (letter = 'a'; letter <= 'z'; letter ; letter++)
         putchar(letter);
+    if(letter !='e' && letter !='q') 
     
     putchar('\n');
     return (0);
