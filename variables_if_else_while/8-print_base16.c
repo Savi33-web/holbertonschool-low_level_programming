f8e23f065e3f9329cd946582eb8afa5c7ebd6a9a
Savi33-web <savinozerk99@gmail.com>
2025-06-20 12:44:19 +0000

    test

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase, followed by a new line 0
+ * Return: Always 0
+ */
+int main(void)
+{
+    char number ;
+
+    for (number = '0'; number <= '15'; number++)
+        putchar(digit);
+
+    putchar('\n');
+    return (0);
+}
