2bde001f831fdacf8f4b6c88712e41637e36aacb
Savi33-web <savinozerk99@gmail.com>
2025-06-23 21:09:12 +0000

    test

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,22 @@
-#include <stdio.h>
+#include <main.h>
 
-/**                                                                                                                                                                                       
- * main - write a program that prints _putchar followed by a new line                                                                                                                     
- *                                                                                                                                                                                        
- * Return: Always 0 (Success)                                                                                                                                                             
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
-  int ch;
-  do
-     (char = "putchar" ; char *str = "_putchar")
-    putchar(ch);
+  /* Display each character from "_putchar"/
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-  while(ch = "putchar")
-{
-  ("putchar") ('\n');
-}
 return (0);
-
 }
-
