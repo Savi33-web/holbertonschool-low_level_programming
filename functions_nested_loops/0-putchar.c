c7522f5c0abede3d86f7f0e530f29b831d7730c4
Savi33-web <savinozerk99@gmail.com>
2025-06-23 08:34:36 +0000

    test

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line.
+ * Return: Always 0
+ */
+
+int main (void)
+
+{
+
+char _putchar
+for (char =  "_putchar")
+{ putchar(_putchar)
+
+putchar ('/n');}
+return (0);
+
+}
