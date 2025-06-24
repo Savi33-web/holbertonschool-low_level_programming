ec627f9b12ae8b769398fd594a3de84930b1bf2b
Savi33-web <savinozerk99@gmail.com>
2025-06-24 12:51:52 +0000

    test

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,19 +7,18 @@
  */
 void print_alphabet_x10(void)
 {
-    int count = 0;
-
-    while (count < 10)
-    {
-        char letter;
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-            _putchar(letter);
-        }
-        _putchar('\n');
-        count++;       
-   }
-    return;
+int count = 0;
+while (count < 10)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+count++;       
+}
+return;
 }
 
 
