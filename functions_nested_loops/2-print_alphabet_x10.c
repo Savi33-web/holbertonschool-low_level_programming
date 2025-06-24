9fea3a0ae1f8689b7bed0dd064d6b3b42adc9ad0
Savi33-web <savinozerk99@gmail.com>
2025-06-24 11:55:49 +0000

    test

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the lowercase alphabet followed by a newline.
+ * Description: This function uses a loop to print all lowercase letters
+ * from 'a' to 'z' using the _putchar function, followed by a newline.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+for(i ='0' ; i < 10 ; i++)
+{
+_putchar(i);
+}
+_putchar('\n');    
+return;
+}
