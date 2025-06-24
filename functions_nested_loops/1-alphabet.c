b7ff8a26b00a9f2ebfa0d7b6cbbaef189449bb6a
Savi33-web <savinozerk99@gmail.com>
2025-06-24 10:03:10 +0000

    test

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,16 +5,15 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
 int i;
- for (i = 'a'; i = <= 'z'; i++)
-{
+ for (i = 'a'; i <= 'z'; i++){
+
 _putchar (i);
 
-}
-
 _putchar('\n');
+ print_alphabet();}
  
-    return 0;
+ return;
 }
