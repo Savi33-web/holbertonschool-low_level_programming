9985c94f766dfa20b44b39ca1c6769c9737bf931
Savi33-web <savinozerk99@gmail.com>
2025-06-24 07:49:10 +0000

    test

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,11 +7,14 @@
  */
 int main(void)
 {
-    print_alphabet();
-  letter;
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-       _putchar(letter);
+   
+  char word[] = "letter";
+    int i= 0;
+  do  (letter = 'a'; letter <= 'z'; letter++)
+	while (word[i] != '\0')
+	  {
+	    _putchar(word[i]);
+	    i++;
     }
    _putchar('\n');
     
