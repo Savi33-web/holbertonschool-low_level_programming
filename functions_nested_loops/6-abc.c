171a527dd080ac1aeaf35edc1bc92503c89b2b18
Savi33-web <saviozerk99@gmail.com>
2025-06-25 13:53:20 +0000

    test

--- functions_nested_loops/6-abc.c
+++ functions_nested_loops/6-abc.c
@@ -8,10 +8,21 @@
  */
 int main(void)
 {
-    int r = -10;
+  int r;
+  int s;
+  int t;
+  int u;
 
+  int r= -1;
+  int s = 0;
+  int t = 1;
+  int u = -98;
+  {
     r = abs(r);
-    printf("%d\n", r);
-
+    _putchar('r');
+    _putchar('s');
+    _putchar('t');
+    _putchar('u');
+  }
     return 0;
 }
