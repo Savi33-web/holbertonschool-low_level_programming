0190223da679875a523ca9416f147f6d7e7f973e
Savi33-web <saviozerk99@gmail.com>
2025-06-25 11:40:27 +0000

    test

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -7,34 +7,39 @@
  * Returns 0 and prints 0 if n is zero
  * Returns -1 and prints - if n is less than zero
  */
-int main(void)
+int print_sign(int n)
 {
 int r;
 int n;
-if ('n' > 0);
+{
+if ('n' > 0)
 return 1;
-if else('n' = 0);
+if else('n' = 0)
 return 0;
-else('n' <= -1);
-
- r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
+else('n' <= -1)
+return -1; 
+}
+{
+r = print_sign(98);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+r = print_sign(0);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+r = print_sign(0xff);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+r = print_sign(-1);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+}
+return;
 }
