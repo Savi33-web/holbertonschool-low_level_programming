60d102020ed7c995cd654275b2b536b37e51028b
Savi33-web <saviozerk99@gmail.com>
2025-06-25 13:11:56 +0000

    test

---
+++ functions_nested_loops/6-abc.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Write a function that computes the absolute value of an integer.
+ *
+ * Return: Always 0.
+ */
+int main(void){
+int r;
+int r = 10;
+ 
+
+ r = abs(('r');
+
+
+return;
+}
