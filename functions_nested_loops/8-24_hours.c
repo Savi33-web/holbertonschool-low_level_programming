0ae96de3f66137c6d1f06b4fecd79e3ad1fa9967
Savi33-web <savinozerk99@gmail.com>
2025-06-26 07:43:25 +0000

    test

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
- * @n: the integer to extract the hour from 00:00 to 23:59.
- * Return: the value of the hours of the day.
+ * main - Write a function that prints every minute of the day of Jack Bauer,
+ *        starting from 00:00 to 23:59.
  */
+
 void jack_bauer(void)
 
 {
