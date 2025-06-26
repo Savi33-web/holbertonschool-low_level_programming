a367f53b73bdffcceb00e4e14604d45730910d51
Savi33-web <savinozerk99@gmail.com>
2025-06-26 13:54:14 +0000

    test

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Write a function that prints the 9 times table, starting with 0.
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+  char a,b,c,d,e,f,g,h,i,j;
+  char str[a,b,c,d,e,f,g,h,i,j]= times_table;
+
+  {
+    for (
+
+
+  times_table();
+    return (0);
+}
