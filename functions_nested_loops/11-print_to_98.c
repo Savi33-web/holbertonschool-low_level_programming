fa354bb8a7c8450eb713f639735ec24efee0b560
Savi33-web <savinozerk99@gmail.com>
2025-06-27 08:42:22 +0000

    test

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,33 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_number - prints an integer using only _putchar
+ * @n: the integer to print
  */
-int main(void)
+void print_number(int n)
 {
-  int n;
-
-  for (n = 0; n < 99; n++)
-  {
-    print_number(n);
-      _putchar (',');
-    _putchar (' ');
-  }
-
-      if (n < 0)
+    if (n < 0)
     {
         _putchar('-');
         n = -n;
     }
+
     if (n / 10)
         print_number(n / 10);
+
     _putchar((n % 10) + '0');
-  {
-    int n = 42;
-    print_number(n);
-    _putchar('\n');
-    return (0);
 }
+
    
