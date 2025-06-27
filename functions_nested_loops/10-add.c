59149495fac8133c233c80e6ed2e6deea04045c0
Savi33-web <savinozerk99@gmail.com>
2025-06-27 07:59:45 +0000

    test

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,31 +1,14 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer using _putchar
- * @n: the integer to print
+ * add - adds two integers and returns the result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
  */
-void print_number(int n)
-{return(a + b);
-    }
+int add(int a, int b)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
-    if (n / 10)
-        print_number(n / 10);
-    _putchar((n % 10) + '0');
+    return (a + b);
 }
 
-int mian (void);
-{
-    int result;
-
-    result = add(89, 9);
-
-    print_number(result);
-    _putchar('\n');
-
-    return (0);
-}
