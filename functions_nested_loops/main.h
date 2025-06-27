3a597f4294cee118b056350ae3eecbbc447be18c
Savi33-web <savinozerk99@gmail.com>
2025-06-27 07:45:18 +0000

    test

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,8 +7,8 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int _putchar(char c);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int a, int b);
 #endif /* MAIN_H */
