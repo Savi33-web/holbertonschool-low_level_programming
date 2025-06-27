2127ec5165ef1c89415f5c093ba95d06e0cd30d2
Savi33-web <savinozerk99@gmail.com>
2025-06-27 11:49:20 +0000

    test

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,32 +1,18 @@
 #include "main.h"
 
-
 /**
- * main -  Declare _putchar if not declared in any header
- * Return: Always 0
+ * _isupper - Checks for uppercase character.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
  */
+int _isupper(int c)
 {
-int _isupper(int c);
-char c;
-
-    
-    c = 'A';
-    { if ((c >= 'A' && c <= 'Z'))
-        _putchar('1');}
-    {
+    if (c >= 'A' && c <= 'Z')
+        return 1;
     else
-        _putchar('0');
-    }
-    {
-    c = 'a';
-    if ((c >= 'a' && c <= 'z'))
-      _putchar('1');}
-    else 
-    {        _putchar('0');
-
-    _putchar('\n');
-    }
-    return (0);
+        return 0;
 }
+
 
 
