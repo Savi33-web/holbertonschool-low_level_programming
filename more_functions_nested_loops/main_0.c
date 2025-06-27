769a0ff819dced3251e5b5ecff171e72ab818aef
Savi33-web <savinozerk99@gmail.com>
2025-06-27 12:20:05 +0000

    lol

--- more_functions_nested_loops/main_0.c
+++ more_functions_nested_loops/main_0.c
@@ -10,5 +10,12 @@ int main(void)
     _putchar(_isupper('G') + '0'); // prints '1'
     _putchar(_isupper('g') + '0'); // prints '0'
     _putchar('\n');
+    int c = '5';
+    if (_isdigit(c))
+        printf("%c is a digit\n", c);
+    else
+        printf("%c is not a digit\n", c);
+
     return (0);
+
 }
